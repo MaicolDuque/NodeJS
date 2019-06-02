2a85208b7fa59a873eacd837fe06eaf122bb81b7
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2019-06-01 22:45:46 -0500

    Archivos estaticos

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello <b>Maicol</b>')
-})
+
+app.use(express.static(__dirname + "/public"))
  
 app.listen(3000)
